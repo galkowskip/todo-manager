2750eb54adf9002a8539cadd63bb2cca8531eb63
galkowski.p <pawel.g@rsstudio.net>
2023-11-02 12:10:55 +0100

    init commit

---
+++ controllers/todos.ts
@@ -0,0 +1,29 @@
+// todos controller
+
+const GetTodos = async () => {
+    // get list of todos
+}
+
+const GetTodo = async (id: String) => {
+    // get a todo
+}
+
+const CreateTodo = async (todo: Todo) => {
+    // create a todo
+}
+
+const UpdateTodo = async (id: String, todo: Todo) => {
+    // update a todo
+}
+
+const DeleteTodo = async (id: String) => {
+    // delete a todo
+}
+
+export {
+    GetTodos,
+    GetTodo,
+    CreateTodo,
+    UpdateTodo,
+    DeleteTodo
+}
---
+++ routes/todos.ts
@@ -0,0 +1,38 @@
+import {Router} from "express"
+
+import { GetTodos, GetTodo, CreateTodo, UpdateTodo, DeleteTodo } from "../controllers/todos"
+
+const router = Router()
+
+router.get('/', async (_, res) => {
+    const todos = await GetTodos()
+    res.send(todos)
+})
+
+router.get('/:id', async (req, res) => {
+    const id = req.params.id
+    const todo = await GetTodo(id)
+    res.send(todo)
+})
+
+router.post('/', async (req, res) => {
+    const todo = req.body
+    const newTodo = await CreateTodo(todo)
+    res.send(newTodo)
+})
+
+router.put('/:id', async (req, res) => {
+    const id = req.params.id
+    const todo = req.body
+    
+    const updatedTodo = await UpdateTodo(id, todo,)
+    res.send(updatedTodo)
+})
+
+router.delete('/:id', async (req, res) => {
+    const id = req.params.id
+    const isDeleted = await DeleteTodo(id)
+    res.send(isDeleted)
+})
+
+export default router
